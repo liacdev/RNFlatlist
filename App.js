8f7c0bc7e7a4fd103368c1e5527b36dda37ac38b
Jay For Short <jagoblacksky@gmail.com>
2021-09-01 20:23:36 -0500

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { MainList } from "./src/components/mainlist.js";
+import { SecondList } from "./src/components/secondlist";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <MainList />
+
+      <SecondList />
+    </>
+    // <View style={styles.container}>
+    //  <Text>Replace this with a Flatlist!</Text>
+    //  <StatusBar style="auto" />
+    // </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
